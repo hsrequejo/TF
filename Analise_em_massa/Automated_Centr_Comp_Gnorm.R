1817987d615a5cf68d2a2dfe18e189ccef8763ed
hsrequejo <hsrequejo@gmail.com>
2021-02-03 15:36:54 -0300

    Adicionado a comparação de similaridade entre Gnorm e centralidade para as redes aleatórias

--- Analise_em_massa/Automated_Centr_Comp_Gnorm.R
+++ Analise_em_massa/Automated_Centr_Comp_Gnorm.R
@@ -1,3 +1,5 @@
+library(fs)
+
 
 CentrCompare = function(rData_centr_path = "./RDatas_AllCentr/ants_allCentr.RData", ranking_cutoff = 10){
   
@@ -66,8 +68,66 @@ CentrCompare = function(rData_centr_path = "./RDatas_AllCentr/ants_allCentr.RDat
 # informa o R que o diretório do documento atual 'e o diretorio de trabalho
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Gera uma lista com os caminhos dos RDatas do Gnorm
-rData_AllCentr_path_list <- dir_ls("./RDatas_AllCentr", glob = "*.RData")
+index = c("2_100_20", "2_100_30", "2_100_50",
+          "2_250_50", "2_250_75", "2_250_125",
+          "3_100_20", "3_100_30", "3_100_50",
+          "3_250_50", "3_250_75", "3_250_125",
+          "5_100_20", "5_100_30", "5_100_50",
+          "5_250_50", "5_250_75", "5_250_125")
+
+for (i in 1:length(index)) {
+  dir_RData_all_Centr = paste("./RDatas_", index[i], "_allCentr", sep = "")
+  rData_to_save = paste(index[i] ,"_Similaridade", sep = "")
+  # Gera uma lista com os caminhos dos RDatas do Gnorm. Avalia todos os arquivos *.RData do diretorio
+  rData_AllCentr_path_list <- dir_ls(dir_RData_all_Centr, glob = "*.RData")
+  
+  simil_bin = data.frame(clo=numeric(),
+                         btw=numeric(), 
+                         eig=numeric(),
+                         deg=numeric(),
+                         Gnorm=numeric()) 
+  
+  simil_dist = data.frame(clo=numeric(),
+                          btw=numeric(), 
+                          eig=numeric(),
+                          deg=numeric(),
+                          Gnorm=numeric()) 
+  
+  for (i in 1:length(rData_AllCentr_path_list)) {
+    centr_comp = CentrCompare(rData_centr_path = rData_AllCentr_path_list[[i]], ranking_cutoff = 10)
+    simil_bin = rbind(simil_bin, centr_comp[[1]])
+    simil_dist = rbind(simil_dist, centr_comp[[2]])
+  }
+  
+  colnames(simil_bin) = c("Closeness", "Betweeness", "Eingen Vector", "Degree", "Gnorm")
+  colnames(simil_dist) = c("Closeness", "Betweeness", "Eingen Vector", "Degree", "Gnorm")
+  simil_dist
+  simil_bin
+  
+  simil_dist_mean = sapply(simil_dist, mean, na.rm = TRUE)
+  simil_dist_sd = sapply(simil_dist, sd, na.rm = TRUE)
+  simil_bin_mean = sapply(simil_bin, mean, na.rm = TRUE)
+  simil_bin_sd = sapply(simil_bin, sd, na.rm = TRUE)
+  
+  simil_dist_mean_sd = rbind(simil_dist_mean, simil_dist_sd)
+  rownames(simil_dist_mean_sd) = c("Mean", "SD")
+  simil_bin_mean_sd = rbind(simil_bin_mean, simil_bin_sd)
+  rownames(simil_bin_mean_sd) = c("Mean", "SD")
+  
+  
+  save(simil_bin, simil_dist,
+       simil_bin_mean_sd, simil_dist_mean_sd,
+       file = paste("./RDatas_Similaridade/", rData_to_save, ".RData", sep = ""))
+  
+}
+
+# informa o R que o diretório do documento atual 'e o diretorio de trabalho
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# faz uma vez apenas
+dir_RData_all_Centr = "./RDatas_allCentr"
+rData_to_save = "allNets_Similaridade"
+# Gera uma lista com os caminhos dos RDatas do Gnorm. Avalia todos os arquivos *.RData do diretorio
+rData_AllCentr_path_list <- dir_ls(dir_RData_all_Centr, glob = "*.RData")
 
 simil_bin = data.frame(clo=numeric(),
                        btw=numeric(), 
@@ -84,10 +144,26 @@ simil_dist = data.frame(clo=numeric(),
 for (i in 1:length(rData_AllCentr_path_list)) {
   centr_comp = CentrCompare(rData_centr_path = rData_AllCentr_path_list[[i]], ranking_cutoff = 10)
   simil_bin = rbind(simil_bin, centr_comp[[1]])
-  simil_dist = rbind(simil_bin, centr_comp[[2]])
+  simil_dist = rbind(simil_dist, centr_comp[[2]])
 }
 
-colnames(simil_bin) = c("clo", "btw", "eig", "deg", "Gnorm")
-colnames(simil_dist) = c("clo", "btw", "eig", "deg", "Gnorm")
+colnames(simil_bin) = c("Closeness", "Betweeness", "Eingen Vector", "Degree", "Gnorm")
+colnames(simil_dist) = c("Closeness", "Betweeness", "Eingen Vector", "Degree", "Gnorm")
 simil_dist
 simil_bin
+
+simil_dist_mean = sapply(simil_dist, mean, na.rm = TRUE)
+simil_dist_sd = sapply(simil_dist, sd, na.rm = TRUE)
+simil_bin_mean = sapply(simil_bin, mean, na.rm = TRUE)
+simil_bin_sd = sapply(simil_bin, sd, na.rm = TRUE)
+
+simil_dist_mean_sd = rbind(simil_dist_mean, simil_dist_sd)
+rownames(simil_dist_mean_sd) = c("Mean", "SD")
+simil_bin_mean_sd = rbind(simil_bin_mean, simil_bin_sd)
+rownames(simil_bin_mean_sd) = c("Mean", "SD")
+
+
+save(simil_bin, simil_dist,
+     simil_bin_mean_sd, simil_dist_mean_sd,
+     file = paste("./RDatas_Similaridade/", rData_to_save, ".RData", sep = ""))
+
